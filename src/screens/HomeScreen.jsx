70aac9bc9e37a6c90c5e55e0885abb63f52701fe
Anas Firly <anasfirly20@gmail.com>
2023-09-13 23:21:25 +0400

    adding homescreen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text } from "react-native";
+
+// Styles
+import { homeStyle } from "../styles/Home.js";
+
+// Miscellaneous
+import { SvgXml } from "react-native-svg";
+import { brandLogo } from "../assets/icons/Brand";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#393e5d" }}>
+      <View>
+        <Text style={homeStyle.headerText}>Розыгрыш</Text>
+        <SvgXml xml={brandLogo(160, 160)} style={homeStyle.logoContainer} />
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ src/styles/Home.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const homeStyle = StyleSheet.create({
+  headerText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "500",
+    marginTop: 15,
+    color: "white",
+  },
+  logoContainer: {
+    alignSelf: "center",
+    marginTop: 20,
+  },
+});
