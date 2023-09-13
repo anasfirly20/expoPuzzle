b19bab7f01938c88f862a82d5679f5a8d7e93843
Anas Firly <anasfirly20@gmail.com>
2023-09-13 17:05:43 +0400

    adding screens

---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
